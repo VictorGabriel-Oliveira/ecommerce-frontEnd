85e2574dd565937973d979dbb160ba095ec8021a
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-11-26 10:37:21 -0300

    adding price component

---
+++ src/components/Price.js
@@ -0,0 +1,11 @@
+import '../styles/price.scss'
+const Price = () => {
+    return (
+        <div className="price_conteiner">
+            <div className="last_price">$125,00</div>
+            <div className="off">50%</div>
+            <div className="initial_price">$250,00</div>
+        </div>
+    )
+}
+export default Price
